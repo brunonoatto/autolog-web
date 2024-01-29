e34f04e44eed9110cd851c6b39c4a9aca4bb1c24
brunonoatto <brunonoatto@gmail.com>
2024-01-29 16:32:24 -0300

    Instalação do ESLint e Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,33 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
+    'eslint-config-prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      typescript: {}, // this loads <rootdir>/tsconfig.json to eslint
+    },
   },
-}
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: true,
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+  },
+};
--- src/core/router/index.tsx
+++ src/core/router/index.tsx
@@ -1,23 +1,22 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from 'react-router-dom';
 
-import ChoosePerfil from "@choose-perfil/index";
-import Driver from "@driver/index";
-import ServiceProvider from "@service-provider/index";
+import ChoosePerfil from '@choose-perfil/index';
+import Driver from '@driver/index';
+import ServiceProvider from '@service-provider/index';
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <ChoosePerfil />,
-    },
-    {
-        path: "/prestador-servico",
-        element: <ServiceProvider />,
-    },
-    {
-        path: "/motorista",
-        element: <Driver />,
-    },
-
+  {
+    path: '/',
+    element: <ChoosePerfil />,
+  },
+  {
+    path: '/prestador-servico',
+    element: <ServiceProvider />,
+  },
+  {
+    path: '/motorista',
+    element: <Driver />,
+  },
 ]);
 
 export default router;
