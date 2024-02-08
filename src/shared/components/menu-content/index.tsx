ccc5a331f3b1fdb627954fce0ed5f32d25f9f94c
brunonoatto <brunonoatto@gmail.com>
2024-02-08 11:40:53 -0300

    Adicionada funcionalidade para fechar o MenuContent quando tiver um clique fora do elemento

--- src/shared/components/menu-content/index.tsx
+++ src/shared/components/menu-content/index.tsx
@@ -1,6 +1,8 @@
 import { useNavigate } from 'react-router-dom';
 
 import IconButton from '../icon-button';
+import { useRef } from 'react';
+import useOutsideClick from '@shared/hooks/useOutsideClick';
 
 type TMenuContentItem = {
   route?: string;
@@ -13,33 +15,53 @@ type TMenuContentProps = React.HTMLAttributes<HTMLButtonElement> & {
 };
 
 const MenuContent = ({ items, ...otherPops }: TMenuContentProps) => {
+  const ref = useRef<HTMLDivElement>(null);
   const navigate = useNavigate();
-  const onToogleContentClick = () => {
+
+  const handleToogleContentClick = () => {
     const menuList = document.getElementById('menu-content');
     menuList?.classList.toggle('hidden');
   };
 
-  const onMenuClick =
+  const handleCloseMenu = () => {
+    const menuList = document.getElementById('menu-content');
+    menuList?.classList.add('hidden');
+  };
+
+  const handleMenuClick =
     (route: TMenuContentItem['route'], onClick: TMenuContentItem['onClick']): TMenuContentItem['onClick'] =>
     (e) => {
-      if (route) navigate(route);
       onClick?.(e);
+
+      if (route) {
+        navigate(route);
+      } else {
+        handleCloseMenu();
+      }
     };
 
-  return (
-    <>
-      <IconButton id="dropdownDefaultButton" icon="ArrowDownLineIcon" onClick={onToogleContentClick} {...otherPops} />
+  useOutsideClick({ ref, action: handleCloseMenu });
 
-      <div id="menu-content" className="hidden min-w-36 absolute top-14 right-3 rounded-lg bg-neutral-700 p-2">
-        <ul className="text-md text-neutral-200 space-y-2" aria-labelledby="dropdownDefaultButton">
-          {items.map(({ title, route, dividerTop, onClick }) => (
-            <li key={route} className={`${dividerTop ? 'border-t-2 border-teal-800' : ''}`}>
-              <button onClick={onMenuClick(route, onClick)}>{title}</button>
-            </li>
-          ))}
-        </ul>
+  return (
+    <div ref={ref}>
+      <IconButton id="menu-content-button" icon="ArrowDownLineIcon" onClick={handleToogleContentClick} {...otherPops} />
+
+      <div
+        id="menu-content"
+        className="absolute hidden min-w-36 top-14 right-3 p-2 text-md font-semibold text-neutral-200 bg-neutral-700 space-y-1"
+      >
+        {items.map(({ title, route, dividerTop, onClick }) => (
+          <div key={route} className={`py-1 ${dividerTop ? 'border-t-2 border-teal-800' : ''}`}>
+            <button
+              className={`block w-full   p-2 rounded-xl ring-teal-300 hover:ring-2`}
+              onClick={handleMenuClick(route, onClick)}
+            >
+              {title}
+            </button>
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
--- src/shared/components/portal/index.tsx
+++ src/shared/components/portal/index.tsx
@@ -4,7 +4,8 @@ type TPortalProps = PropsWithChildren & { open: boolean };
 const Portal = ({ children, open }: TPortalProps) => {
   if (!open) return null;
 
-  return <div className="absolute inset-0 bg-neutral-100 bg-opacity-20 z-50">{children}</div>;
+  // TODO: absolute tbm funciona
+  return <div className="fixed inset-0 bg-neutral-100 bg-opacity-20">{children}</div>;
 };
 
 export default Portal;
