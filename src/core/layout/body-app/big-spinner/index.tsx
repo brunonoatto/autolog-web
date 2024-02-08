fc6864108f58cca297bab257d5f4e465980607fb
brunonoatto <brunonoatto@gmail.com>
2024-02-08 11:32:42 -0300

    fix

--- src/core/layout/body-app/big-spinner/index.tsx
+++ src/core/layout/body-app/big-spinner/index.tsx
@@ -6,7 +6,7 @@ const BigSpinner = () => {
   const isLoading = useLoadingStore((props) => props.isLoading);
 
   return (
-    <Portal open={true || isLoading}>
+    <Portal open={isLoading}>
       <div className="relative top-1/4 m-auto w-48 bg-neutral-800 text-center rounded-xl py-4">
         <Icon icon="LoadingIcon" />
         <div className="text-teal-400">Carregando...</div>
