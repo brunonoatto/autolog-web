0bd542ecd0f4b3a58c430d3bed58bb6552927c5a
brunonoatto <brunonoatto@gmail.com>
2024-02-08 18:50:17 -0300

    fix layout

--- src/core/layout/body-app/big-spinner/index.tsx
+++ src/core/layout/body-app/big-spinner/index.tsx
@@ -7,7 +7,7 @@ const BigSpinner = () => {
 
   return (
     <Portal open={isLoading}>
-      <div className="relative top-1/4 m-auto w-48 bg-neutral-800 text-center rounded-xl py-4">
+      <div className="relative top-1/3 m-auto w-48 bg-neutral-800 text-center rounded-xl py-4">
         <Icon icon="LoadingIcon" />
         <div className="text-teal-400">Carregando...</div>
       </div>
--- src/shared/components/portal/index.tsx
+++ src/shared/components/portal/index.tsx
@@ -4,8 +4,7 @@ type TPortalProps = PropsWithChildren & { open: boolean };
 const Portal = ({ children, open }: TPortalProps) => {
   if (!open) return null;
 
-  // TODO: absolute tbm funciona
-  return <div className="fixed inset-0 bg-neutral-100 bg-opacity-20">{children}</div>;
+  return <div className="fixed inset-0 bg-neutral-100/20">{children}</div>;
 };
 
 export default Portal;
