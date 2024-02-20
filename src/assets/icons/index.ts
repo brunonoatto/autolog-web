b4db4f50404fddb46298918d31e51f46e0312ae9
brunonoatto <brunonoatto@gmail.com>
2024-02-20 09:07:11 -0300

    feat: Inclusão de novos icones

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,8 @@
 export { default as ArrowDownLineIcon } from './components/ArrowDownLineIcon';
+export { default as CheckCircleIcon } from './components/CheckCircleIcon';
+export { default as CloseCircleIcon } from './components/CloseCircleIcon';
+export { default as FileExclamationIcon } from './components/FileExclamationIcon';
 export { default as LoadingIcon } from './components/LoadingIcon';
 export { default as MenuIcon } from './components/MenuIcon';
+export { default as ProgressWrenchIcon } from './components/ProgressWrenchIcon';
+export { default as SendIcon } from './components/SendIcon';
