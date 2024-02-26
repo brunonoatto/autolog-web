827eaa638245883599ebaa637b9214dbd4789eb8
brunonoatto <brunonoatto@gmail.com>
2024-02-26 15:15:09 -0300

    feat: criação do botão secundário

--- src/shared/design-system/button/index.tsx
+++ src/shared/design-system/button/index.tsx
@@ -1,10 +1,20 @@
-const Button = ({ className = '', ...otherProps }: React.HTMLAttributes<HTMLButtonElement>) => {
-  return (
-    <button
-      className={`text-white bg-teal-800 hover:bg-teal-700 focus:ring-4 focus:ring-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none ${className}`}
-      {...otherProps}
-    />
-  );
+type TButtonProps = React.HTMLAttributes<HTMLButtonElement> & { color?: 'primary' | 'secondary' };
+const Button = ({ className = '', color = 'primary', ...otherProps }: TButtonProps) => {
+  if (color === 'primary') {
+    return (
+      <button
+        className={`text-white bg-teal-800 hover:bg-teal-700 focus:ring-4 focus:ring-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none ${className}`}
+        {...otherProps}
+      />
+    );
+  } else {
+    return (
+      <button
+        className={`text-white hover:text-teal-300 ring-1 ring-white hover:ring-teal-300 focus:ring-4 focus:ring-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none ${className}`}
+        {...otherProps}
+      />
+    );
+  }
 };
 
 export default Button;
