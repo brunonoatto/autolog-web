9b510ae76ab03b5e9b84a9d1dafed44a2e94878d
brunonoatto <brunonoatto@gmail.com>
2024-03-04 23:02:51 -0300

    feat: realizadas configurações padrão no react query

--- src/app.tsx
+++ src/app.tsx
@@ -2,13 +2,20 @@ import Router from '@core/router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <Router spinner={<>Big spinner</>} />
-      <ReactQueryDevtools initialIsOpen={false} />
+      <ReactQueryDevtools buttonPosition="bottom-left" />
     </QueryClientProvider>
   );
 };
