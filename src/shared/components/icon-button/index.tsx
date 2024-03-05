92c866c58e5647fa2c2942a805ef9f255c0a9580
brunonoatto <brunonoatto@gmail.com>
2024-03-04 23:21:05 -0300

    fix: ajust para o className não ficar undefined no html

--- src/shared/components/icon-button/index.tsx
+++ src/shared/components/icon-button/index.tsx
@@ -5,8 +5,8 @@ type TIconButtonProps = React.HTMLAttributes<HTMLButtonElement> & {
   iconProps?: Omit<TIconProps, 'name'>;
 };
 
-const IconButton = ({ icon, className, iconProps = {}, ...otherProps }: TIconButtonProps) => {
-  const { className: classNameIcon, ...otherIconProps } = iconProps;
+const IconButton = ({ icon, className = '', iconProps = {}, ...otherProps }: TIconButtonProps) => {
+  const { className: classNameIcon = '', ...otherIconProps } = iconProps;
   return (
     <button className={`rounded  ${className}`} {...otherProps}>
       <Icon name={icon} className={`${classNameIcon} hover:fill-teal-500`} {...otherIconProps} />
--- src/shared/components/link-button/index.tsx
+++ src/shared/components/link-button/index.tsx
@@ -1,7 +1,10 @@
 import { FunctionComponent } from 'react';
 import { Link } from 'react-router-dom';
 
-const LinkButton: FunctionComponent<React.ComponentProps<typeof Link>> = ({ className = '', ...otherProps }) => {
+const LinkButton: FunctionComponent<React.ComponentProps<typeof Link>> = ({
+  className = '',
+  ...otherProps
+}) => {
   return (
     <Link
       className={`text-center bg-teal-800 hover:bg-teal-700 focus:ring-4 focus:ring-teal-300 font-medium rounded-lg text-sm px-3 py-2.5 focus:outline-none ${className}`}
