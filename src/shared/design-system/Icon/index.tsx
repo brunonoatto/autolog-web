11cabb8f3519d3048ebcfabb6db274ed56b4e0e9
brunonoatto <brunonoatto@gmail.com>
2024-03-07 19:31:01 -0300

    refactor: melhoria no type do size do Icon

--- src/shared/design-system/Icon/index.tsx
+++ src/shared/design-system/Icon/index.tsx
@@ -1,13 +1,13 @@
 import * as icons from '@shared/design-system/assets/icons';
 import { TIcons } from '@shared/design-system/assets/icons/types';
 
-const sizeNumber = {
+type TSize = 'small' | 'medium' | 'large';
+
+const sizeData: { [key in TSize]: number } = {
   small: 24,
   medium: 32,
   large: 46,
 };
-
-type TSize = 'small' | 'medium' | 'large';
 
 export type TIconProps = Omit<React.SVGProps<SVGSVGElement>, 'size'> & {
   name: TIcons;
@@ -16,7 +16,7 @@ export type TIconProps = Omit<React.SVGProps<SVGSVGElement>, 'size'> & {
 const Icon = ({ name, size = 'medium', ...otherProps }: TIconProps) => {
   const SvgIcon = icons[name];
 
-  return <SvgIcon width={sizeNumber[size]} height={sizeNumber[size]} {...otherProps} />;
+  return <SvgIcon width={sizeData[size]} height={sizeData[size]} {...otherProps} />;
 };
 
 export default Icon;
