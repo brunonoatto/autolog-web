f0b7e4e602be3d3f3ef7cc6d5c5652f55433ca39
brunonoatto <brunonoatto@gmail.com>
2024-04-01 22:22:28 -0300

    chore: adicionada regra do lint onde variáveis que comecem com _ podem não ser usadas

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,6 +27,18 @@ module.exports = {
     'react/display-name': 'off',
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     '@typescript-eslint/no-explicit-any': ['warn'],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        args: 'all',
+        argsIgnorePattern: '^_',
+        caughtErrors: 'all',
+        caughtErrorsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
+    ],
     'sort-imports': [
       'error',
       {
