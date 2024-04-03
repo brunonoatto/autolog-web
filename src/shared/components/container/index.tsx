90b47e80f86939ca1fbe6142d276f6de11ac0157
brunonoatto <brunonoatto@gmail.com>
2024-04-03 18:24:49 -0300

    feat: permitir passar por parâmetro se o Container vai ter borda ou não

--- src/shared/components/container/index.tsx
+++ src/shared/components/container/index.tsx
@@ -5,14 +5,21 @@ import Icon from '@shared/design-system/Icon';
 
 type TContainerProps = {
   children: React.ReactNode;
-  title: string;
-  icon?: TIcons;
   className?: string;
+  border?: boolean;
+  icon?: TIcons;
+  title: string;
 };
 
-export default function Container({ children, title, icon, className }: TContainerProps) {
+export default function Container({ children, className, border, icon, title }: TContainerProps) {
   return (
-    <div className={twMerge('rounded-lg border-2 border-teal-700 p-2 space-y-4', className)}>
+    <div
+      data-border={border}
+      className={twMerge(
+        'rounded-lg data-[border=true]:ring-2 data-[border=true]:ring-teal-700 p-2 space-y-4',
+        className,
+      )}
+    >
       {title && (
         <h2 className="flex gap-2 border-b-2 border-teal-800 w-3/4 pb-1 pl-1">
           {icon && <Icon name={icon} />}
