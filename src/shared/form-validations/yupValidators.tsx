f6ee1db205d3617a9f3db86da17dc3291b52bd0f
brunonoatto <brunonoatto@gmail.com>
2024-04-17 00:14:22 -0300

    feat: adicionada validação do cpf

--- src/shared/form-validations/yupValidators.tsx
+++ src/shared/form-validations/yupValidators.tsx
@@ -15,8 +15,11 @@ export const NumberValidator = () => yup.number().typeError(NUMBER_INVALID_MSG);
 
 export const EmailValidator = () => yup.string().email(EMAIL_INVALID_MSG);
 
+export const CpfValidator = () => yup.string().max(11, MAX_INVALID_MSG(11));
+
 export const CnpjValidator = () => yup.string().max(14, MAX_INVALID_MSG(14));
 
 export const DateValidator = () => yup.date().typeError(DATE_INVALID_MSG);
 
-export const BirthdayValidator = () => DateValidator().min(new Date(1920, 0, 1), BYRTHDAY_INVALID_MSG);
+export const BirthdayValidator = () =>
+  DateValidator().min(new Date(1920, 0, 1), BYRTHDAY_INVALID_MSG);
