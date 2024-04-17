efc09a03086a95f2f2575a6db04fa53347f28550
brunonoatto <brunonoatto@gmail.com>
2024-04-17 17:20:42 -0300

    feat: implementação de novo select q lista todos os carros que o cliente tem ou foram transferidos

---
+++ src/shared/components/selects/client-cars/index.tsx
@@ -0,0 +1,18 @@
+import { forwardRef } from 'react';
+
+import { useClientCars } from '@core/service/client';
+import Select, { type TSelectDefaultProps } from '@shared/design-system/select';
+import { buildSelectOptions } from '@shared/design-system/select/helpers';
+
+const ClientCarSelect = forwardRef<HTMLSelectElement, TSelectDefaultProps>((props, ref) => {
+  const { data: clientCars } = useClientCars({ transfereds: true });
+
+  const options = buildSelectOptions(clientCars, 'license', (item) => {
+    const transferedText = item.isTransfered ? '(Transferido) ' : '';
+    return `${transferedText}${item.license} - ${item.brand} ${item.model} ${item.year}`;
+  });
+
+  return <Select options={options} ref={ref} {...props} />;
+});
+
+export default ClientCarSelect;
--- src/shared/design-system/select/helpers.ts
+++ src/shared/design-system/select/helpers.ts
@@ -3,12 +3,12 @@ import type { TSelectOption } from '.';
 export const buildSelectOptions = <T>(
   options: T[] | undefined,
   valueProp: keyof T,
-  titleProp: keyof T,
+  titleProp: keyof T | ((item: T) => string),
 ): TSelectOption[] => {
   return (
     options?.map((option) => ({
       value: option[valueProp] as string,
-      title: option[titleProp] as string,
+      title: typeof titleProp === 'function' ? titleProp(option) : (option[titleProp] as string),
     })) || []
   );
 };
