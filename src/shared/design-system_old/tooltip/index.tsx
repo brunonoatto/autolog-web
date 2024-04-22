6b1b62b0be6b2dd63c12cdf89504a1c7270dd492
brunonoatto <brunonoatto@gmail.com>
2024-04-22 16:34:45 -0300

    style: ajuste cor de fundo padrão do tooltip

--- src/shared/design-system_old/tooltip/index.tsx
+++ src/shared/design-system_old/tooltip/index.tsx
@@ -7,7 +7,7 @@ export default function Tooltip({ children, title }: TTooltipProps) {
   return (
     <div className="relative group group-hover">
       {children}
-      <div className="hidden group-hover:block text-left absolute bg-zinc-600 ring-1 ring-teal-600 min-w-80 p-2 text-sm">
+      <div className="hidden group-hover:block text-left absolute bg-neutral-900 ring-1 ring-teal-600 min-w-80 p-2">
         {title}
       </div>
     </div>
