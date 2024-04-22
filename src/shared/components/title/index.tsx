9faa0055b894be8530de08edca3976521ffcc82c
brunonoatto <brunonoatto@gmail.com>
2024-04-21 23:24:39 -0300

    style: ajuste estilo do componente title

--- src/shared/components/title/index.tsx
+++ src/shared/components/title/index.tsx
@@ -1,14 +1,22 @@
+import { twMerge } from 'tailwind-merge';
+
 import type { TIcons } from '@shared/design-system_old/assets/icons/types';
 import Icon from '@shared/design-system_old/Icon';
 
 type TTitleProps = {
   children: React.ReactNode;
+  className?: string;
   icon?: TIcons;
 };
 
-export default function Title({ children, icon }: TTitleProps) {
+export default function Title({ children, className, icon }: TTitleProps) {
   return (
-    <h2 className="flex gap-2 border-b-2 border-teal-800 w-11/12 md:w-3/4 pb-1 pl-1">
+    <h2
+      className={twMerge(
+        'text-2xl font-semibold flex gap-2 border-b-[1px] border-primary w-11/12 md:w-3/4 pb-1 pl-1',
+        className,
+      )}
+    >
       {icon && <Icon name={icon} />}
       {children}
     </h2>
