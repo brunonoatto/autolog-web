0e7f763f6ddc4ca43ba2a44152a76f9789dce661
brunonoatto <brunonoatto@gmail.com>
2024-04-21 23:23:05 -0300

    style: ajuste tamanho botão sm

--- src/shared/design-system/ui/button.tsx
+++ src/shared/design-system/ui/button.tsx
@@ -20,7 +20,7 @@ const buttonVariants = cva(
       },
       size: {
         default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
+        sm: 'h-8 rounded-md px-2',
         lg: 'h-11 rounded-md px-8',
         icon: 'h-10 w-10',
       },
@@ -32,13 +32,13 @@ const buttonVariants = cva(
   },
 );
 
-export interface ButtonProps
+export interface TButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+const Button = React.forwardRef<HTMLButtonElement, TButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button';
     return (
