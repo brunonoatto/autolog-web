fd6e8aec849f2abaf56c3434f4a8010d3625e54b
brunonoatto <brunonoatto@gmail.com>
2024-04-22 23:31:06 -0300

    feat: adicionada modal de confirmação de logout

--- src/layout/body-app/header/header-actions/index.tsx
+++ src/layout/body-app/header/header-actions/index.tsx
@@ -1,10 +1,18 @@
+import { useState } from 'react';
+
 import useAuth from '@core/store/context/hooks/useAuth';
 import IconButton from '@shared/design-system/ui/icon-button';
+import Modal from '@shared/design-system/ui/modal';
 
 const HeaderActions = () => {
+  const [openModal, setOpenModal] = useState(false);
   const { logout, getTokenData } = useAuth();
 
   const { name } = getTokenData() || {};
+
+  const handleLogout = () => {
+    logout();
+  };
 
   return (
     <div className="flex gap-2">
@@ -13,7 +21,25 @@ const HeaderActions = () => {
         <div>{name}</div>
       </div>
 
-      <IconButton icon="log-out" variant="ghost" size="icon" title="Sair" onClick={logout} />
+      <IconButton
+        icon="log-out"
+        variant="ghost"
+        size="icon"
+        title="Sair"
+        onClick={() => setOpenModal(true)}
+      />
+
+      <Modal
+        open={openModal}
+        title="Sair do sistema"
+        icon="log-out"
+        confirmText="Sim"
+        onConfirmClick={handleLogout}
+        cancelText="Não"
+        onCancelClick={() => setOpenModal(false)}
+      >
+        Você realmente deseja sair do sistema?
+      </Modal>
     </div>
   );
 };
