c8869b8cf415be4140b7b21fb8cb6225419cdcc3
brunonoatto <brunonoatto@gmail.com>
2024-04-23 21:47:33 -0300

    feat: deixando toda a lógica de dentro do componente Main dentro do mesmo

--- src/layout/body-app/index.tsx
+++ src/layout/body-app/index.tsx
@@ -1,28 +1,20 @@
 import Footer from '@layout/body-app/footer';
 import Header from '@layout/body-app/header';
 import Main from '@layout/body-app/main';
-import Menu from '@layout/menu';
-import { TMenu } from '@layout/menu/types';
+import { TMenu } from '@layout/body-app/main/menu/types';
 
 import styles from './styles.module.css';
 
 type TBodyAppProps = {
-  children: React.ReactNode;
   menus: TMenu[];
 };
 
-export default function BodyApp({ children, menus }: TBodyAppProps) {
+export default function BodyApp({ menus }: TBodyAppProps) {
   return (
     <div className={styles.content}>
       <Header />
 
-      <Main>
-        <div className="md:h-full md:grid md:grid-cols-[15rem_auto]">
-          <Menu items={menus} />
-
-          <div className="overflow-hidden md:overflow-auto p-3">{children}</div>
-        </div>
-      </Main>
+      <Main menus={menus} />
 
       <Footer />
     </div>
--- src/layout/body-app/main/index.tsx
+++ src/layout/body-app/main/index.tsx
@@ -1,10 +1,25 @@
+import { Outlet } from 'react-router-dom';
+
+import Menu from '@layout/body-app/main/menu';
+import type { TMenu } from '@layout/body-app/main/menu/types';
+
 import styles from './styles.module.css';
 
 type TMainProps = {
-  children: React.ReactNode;
+  menus: TMenu[];
 };
 
-const Main = ({ children }: TMainProps) => {
-  return <main className={styles.content}>{children}</main>;
+const Main = ({ menus }: TMainProps) => {
+  return (
+    <main className={styles.content}>
+      <div className="md:h-full md:grid md:grid-cols-[15rem_auto]">
+        <Menu items={menus} />
+
+        <div className="overflow-hidden md:overflow-auto p-3">
+          <Outlet />
+        </div>
+      </div>
+    </main>
+  );
 };
 export default Main;
