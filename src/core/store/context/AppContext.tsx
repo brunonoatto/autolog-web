7ea610a3ff40cdd2b761cf4ffba0dc3ef365f6e8
brunonoatto <brunonoatto@gmail.com>
2024-04-24 18:22:03 -0300

    feat: implementação do interceptor do axios para que os erros retornados do backend sejam exibidos no toaster

--- src/core/store/context/AppContext.tsx
+++ src/core/store/context/AppContext.tsx
@@ -1,11 +1,15 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { createContext } from 'react';
+import type { AxiosError } from 'axios';
+import { createContext, useLayoutEffect } from 'react';
 
+import httpClient from '@core/api/HttpClient';
+import type { TErrorApiData } from '@core/api/types';
 import { AuthProvider } from '@core/store/context/AuthContext';
 import { ThemeProvider } from '@core/store/context/ThemeProvider';
 import BigSpinner from '@layout/body-app/big-spinner';
 import { Toaster } from '@shared/design-system/ui/toaster';
+import { useToast } from '@shared/design-system/ui/use-toast';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,6 +23,36 @@ const queryClient = new QueryClient({
 export const AppContext = createContext({});
 
 export function AppProvider({ children }: { children: React.ReactNode }) {
+  const { toast } = useToast();
+
+  useLayoutEffect(() => {
+    const responseInterceptorId = httpClient.interceptors.response.use(
+      (response) => response,
+      async (error: AxiosError<TErrorApiData>) => {
+        if (error.code === 'ERR_NETWORK') {
+          toast({ title: 'Nâo foi possível conectar com o servidor!', variant: 'destructive' });
+          return;
+        }
+
+        const status = error.response?.status || 0;
+        if (status >= 400 && status <= 499) {
+          const message = error.response?.data?.message;
+
+          if (message) {
+            toast({ title: message, variant: 'destructive' });
+            return;
+          }
+        }
+
+        return Promise.reject(error);
+      },
+    );
+
+    return () => {
+      httpClient.interceptors.response.eject(responseInterceptorId);
+    };
+  }, []);
+
   return (
     <AppContext.Provider value={{}}>
       <QueryClientProvider client={queryClient}>
