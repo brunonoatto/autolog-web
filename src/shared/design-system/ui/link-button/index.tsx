8438463bc53086da476545a255bebab95d3120d4
brunonoatto <brunonoatto@gmail.com>
2024-04-24 17:05:36 -0300

    feat: permitir enviar um icone para o LinkButton

--- src/shared/design-system/ui/link-button/index.tsx
+++ src/shared/design-system/ui/link-button/index.tsx
@@ -3,10 +3,13 @@ import { Link } from 'react-router-dom';
 
 import type { TRoute } from '@core/router/consts';
 import { buttonVariants, type TButtonProps } from '@shared/design-system/ui/button';
+import Icon, { TIconProps, TIcons } from '@shared/design-system/ui/icon';
 
 type TLinkButtonProps = Omit<React.ComponentProps<typeof Link>, 'to'> &
   Pick<TButtonProps, 'variant' | 'size'> & {
     to: TRoute;
+    icon?: TIcons;
+    iconProps?: TIconProps;
   };
 
 // TODO: tentar colocar os estilos do botão aqui
@@ -14,9 +17,21 @@ const LinkButton: FunctionComponent<TLinkButtonProps> = ({
   className = '',
   variant,
   size,
+  icon,
+  iconProps,
+  children,
   ...otherProps
 }) => {
-  return <Link className={buttonVariants({ variant, size, className })} {...otherProps} />;
+  return (
+    <Link className={buttonVariants({ variant, size, className })} {...otherProps}>
+      <div className="flex gap-2 items-center">
+        {icon && (
+          <Icon name={icon as TIcons} size={size === 'sm' ? '1.2rem' : '1.5rem'} {...iconProps} />
+        )}
+        {children}
+      </div>
+    </Link>
+  );
 };
 
 export default LinkButton;
