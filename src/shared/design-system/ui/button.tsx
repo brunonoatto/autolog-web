eb16fe11696990c40c09f4f6bd18f692bf584572
brunonoatto <brunonoatto@gmail.com>
2024-04-24 23:01:00 -0300

    style: removida div q não era necessária no IconButton e correçãona concatenação da variant com className

--- src/shared/design-system/ui/button.tsx
+++ src/shared/design-system/ui/button.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 import { cn } from '@shared/design-system-utils';
 
 export const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex gap-4 items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
@@ -42,7 +42,7 @@ export const Button = React.forwardRef<HTMLButtonElement, TButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button';
     return (
-      <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
+      <Comp className={cn(buttonVariants({ variant, size }), className)} ref={ref} {...props} />
     );
   },
 );
--- src/shared/design-system/ui/icon-button/index.tsx
+++ src/shared/design-system/ui/icon-button/index.tsx
@@ -9,10 +9,8 @@ export type TIconButtonProps = TButtonProps & {
 const IconButton = ({ children, size, icon, iconProps, ...otherProps }: TIconButtonProps) => {
   return (
     <Button size={size} {...otherProps}>
-      <div className="flex gap-2 items-center">
-        <Icon name={icon} size={size === 'sm' ? '1.2rem' : '1.5rem'} {...iconProps} />
-        {children}
-      </div>
+      <Icon name={icon} size={size === 'sm' ? '1.2rem' : '1.5rem'} {...iconProps} />
+      {children}
     </Button>
   );
 };
