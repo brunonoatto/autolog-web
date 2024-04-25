33d039f8bb8515cb3c1f1a2cc6f147f0c354de31
brunonoatto <brunonoatto@gmail.com>
2024-04-25 00:24:42 -0300

    fix: habilitar o useGetCar somente se o parâmetro tiver valor

--- src/core/service/car/index.ts
+++ src/core/service/car/index.ts
@@ -1,12 +1,12 @@
 import { DefaultError, useMutation, useQuery } from '@tanstack/react-query';
 
 import { ServiceApi } from '@core/api';
-import { TNewCar } from '@core/api/car/types';
-import { TUseTransferCarParams } from '@core/service/car/type';
+import type { TNewCar } from '@core/api/car/types';
+import type { TUseTransferCarParams } from '@core/service/car/type';
 
 export const useGetCar = (license: string) => {
   return useQuery({
-    enabled: false,
+    enabled: !!license,
     queryKey: ['useGetCar', license],
     queryFn: async () => {
       const { data } = await ServiceApi.CarApi.get(license);
