c5c67aa959eadaf67ffe915b405ebc67ed8b4def
brunonoatto <brunonoatto@gmail.com>
2024-04-25 18:25:51 -0300

    style: padronização do layout do Dashboard da Garage

--- src/modules/garage/dashboard/index.tsx
+++ src/modules/garage/dashboard/index.tsx
@@ -1,6 +1,6 @@
 import { useListDashboard } from '@core/service/dashboard';
 import { Alert, AlertDescription, AlertTitle } from '@shared/design-system/ui/alert';
-import { CardTitle } from '@shared/design-system/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@shared/design-system/ui/card';
 import LinkButton from '@shared/design-system/ui/link-button';
 import useNavigateApp from '@shared/hooks/useNavigateApp';
 
@@ -18,47 +18,55 @@ export default function Dashboard() {
   };
 
   return (
-    <div className="space-y-4">
-      <CardTitle icon="trello">Dashboard</CardTitle>
+    <Card>
+      <CardHeader>
+        <CardTitle icon="trello">Dashboard</CardTitle>
+      </CardHeader>
 
-      {!isLoadingDashboard && !dashboardItem?.length && (
-        <>
-          <Alert>
-            <AlertTitle>Nenhum orçamento em andamento.</AlertTitle>
-            <AlertDescription>
-              <LinkButton
-                className="hidden md:inline-block"
-                to="/garage/orcamento"
-                icon="circle-dollar-sign"
-              >
-                Adicionar orçamento
-              </LinkButton>
-            </AlertDescription>
-          </Alert>
-        </>
-      )}
-
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {isLoadingDashboard ? (
+      <CardContent>
+        {!isLoadingDashboard && !dashboardItem?.length && (
           <>
-            <StatusCardSkeleton />
-            <StatusCardSkeleton />
-            <StatusCardSkeleton />
-            <StatusCardSkeleton />
-            <StatusCardSkeleton />
-          </>
-        ) : (
-          <>
-            <LinkButton className="md:hidden h-16" to="/garage/orcamento">
-              Adicionar Orçamento
-            </LinkButton>
-
-            {dashboardItem?.map((item) => (
-              <StatusCard key={item.license} item={item} onClick={() => handleSelectCar(item.os)} />
-            ))}
+            <Alert>
+              <AlertTitle>Nenhum orçamento em andamento.</AlertTitle>
+              <AlertDescription>
+                <LinkButton
+                  className="hidden md:inline-block"
+                  to="/garage/orcamento"
+                  icon="circle-dollar-sign"
+                >
+                  Adicionar orçamento
+                </LinkButton>
+              </AlertDescription>
+            </Alert>
           </>
         )}
-      </div>
-    </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {isLoadingDashboard ? (
+            <>
+              <StatusCardSkeleton />
+              <StatusCardSkeleton />
+              <StatusCardSkeleton />
+              <StatusCardSkeleton />
+              <StatusCardSkeleton />
+            </>
+          ) : (
+            <>
+              <LinkButton className="md:hidden h-16" to="/garage/orcamento">
+                Adicionar Orçamento
+              </LinkButton>
+
+              {dashboardItem?.map((item) => (
+                <StatusCard
+                  key={item.license}
+                  item={item}
+                  onClick={() => handleSelectCar(item.os)}
+                />
+              ))}
+            </>
+          )}
+        </div>
+      </CardContent>
+    </Card>
   );
 }
