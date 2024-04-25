5fdcf4d912522f1688f6d409bd9a6479bf3d3c86
brunonoatto <brunonoatto@gmail.com>
2024-04-24 23:01:40 -0300

    style: removida div q não era necessária no LinkButton e correçãona concatenação da variant com className

--- src/shared/design-system/ui/link-button/index.tsx
+++ src/shared/design-system/ui/link-button/index.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import type { TRoute } from '@core/router/consts';
 import { buttonVariants, type TButtonProps } from '@shared/design-system/ui/button';
 import Icon, { TIconProps, TIcons } from '@shared/design-system/ui/icon';
+import { cn } from '@shared/design-system-utils';
 
 type TLinkButtonProps = Omit<React.ComponentProps<typeof Link>, 'to'> &
   Pick<TButtonProps, 'variant' | 'size'> & {
@@ -23,13 +24,11 @@ const LinkButton: FunctionComponent<TLinkButtonProps> = ({
   ...otherProps
 }) => {
   return (
-    <Link className={buttonVariants({ variant, size, className })} {...otherProps}>
-      <div className="flex gap-2 items-center">
-        {icon && (
-          <Icon name={icon as TIcons} size={size === 'sm' ? '1.2rem' : '1.5rem'} {...iconProps} />
-        )}
-        {children}
-      </div>
+    <Link className={cn(buttonVariants({ variant, size }), className)} {...otherProps}>
+      {icon && (
+        <Icon name={icon as TIcons} size={size === 'sm' ? '1.2rem' : '1.5rem'} {...iconProps} />
+      )}
+      {children}
     </Link>
   );
 };
