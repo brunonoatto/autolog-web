35816e2f43cb86cc9985a7c03d470854ee7ed5fe
brunonoatto <brunonoatto@gmail.com>
2024-04-25 01:40:27 -0300

    refactor: melhorias no componente BudgetCard para receber o garageName também e mostrar os dados de acordo com o tipo de usuário

--- src/shared/components/budget-card/index.tsx
+++ src/shared/components/budget-card/index.tsx
@@ -1,37 +1,54 @@
+import { useMemo } from 'react';
+
 import type { TCar } from '@core/api/car/types';
+import useAuth from '@core/store/context/hooks/useAuth';
 import CarInfo from '@shared/components/car-info';
 import StatusBadge from '@shared/components/status-badge';
 import { CardTitle } from '@shared/design-system/ui/card';
 import { BudgetStatusEnum } from '@shared/types/budgetStatus';
 
+// TODO: melhorar a utilização desse componente pra não precisar ser todos campos opcionais
 type TBudgetCardProps = {
-  createdDate?: string;
-  clientName?: string;
-  status?: BudgetStatusEnum;
   car?: TCar;
+  clientName?: string;
+  garageName?: string;
+  status?: BudgetStatusEnum;
   observation?: string;
+  createdDate?: string;
   hover?: boolean;
 };
 
 export default function BudgetCard({
-  createdDate,
-  status,
-  clientName,
   car,
+  clientName,
+  garageName,
+  status,
   observation,
+  createdDate,
   hover = false,
 }: TBudgetCardProps) {
+  const { getTokenData } = useAuth();
+
+  const tokenData = useMemo(() => getTokenData(), [getTokenData]);
+
   if (!status || !car) return null;
 
   return (
-    <>
+    <div className="space-y-4">
       <div
         data-hover={hover}
         className="w-full rounded-lg ring-1 ring-primary data-[hover=true]:hover:ring-2 p-2 md:p-3 flex flex-col duration-300 data-[hover=true]:hover:scale-[1.01]"
       >
-        {clientName && createdDate && (
+        {tokenData?.type === 'garage' && clientName && (
           <div className="flex justify-between text-sm">
-            <p className="whitespace-nowrap overflow-hidden text-ellipsis">{clientName}</p>
+            <p className="whitespace-nowrap overflow-hidden text-ellipsis">Cliente: {clientName}</p>
+            <p>{createdDate}</p>
+          </div>
+        )}
+
+        {tokenData?.type === 'client' && garageName && (
+          <div className="flex justify-between text-sm">
+            <p className="whitespace-nowrap overflow-hidden text-ellipsis">Ofinina: {garageName}</p>
             <p>{createdDate}</p>
           </div>
         )}
@@ -53,10 +70,10 @@ export default function BudgetCard({
         <>
           <CardTitle icon="notebook-pen" size="lg">
             Observação
-          </CardTitle>{' '}
+          </CardTitle>
           {observation}
         </>
       )}
-    </>
+    </div>
   );
 }
