bbe3dabdf439ed95392ea16c851544225f7257b4
brunonoatto <brunonoatto@gmail.com>
2024-04-26 13:43:52 -0300

    feat: melhoria para a api de Auth retornar o data direto

--- src/core/api/auth/index.ts
+++ src/core/api/auth/index.ts
@@ -1,10 +1,8 @@
-import type { AxiosResponse } from 'axios';
-
 import httpClient from '@core/api/HttpClient';
 
 import type { TLoginParams, TLoginResponse } from './types';
 
-export const login = async (data: TLoginParams): Promise<AxiosResponse<TLoginResponse>> => {
+export const login = async (data: TLoginParams): Promise<TLoginResponse> => {
   const response = await httpClient.post<TLoginResponse>('/login', data);
-  return response;
+  return response.data;
 };
--- src/core/service/auth/index.ts
+++ src/core/service/auth/index.ts
@@ -1,13 +1,9 @@
-import { DefaultError, useMutation } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 
 import { ServiceApi } from '@core/api';
-import type { TLoginParams, TLoginResponse } from '@core/api/auth/types';
 
 export const useLogin = () => {
-  return useMutation<TLoginResponse, DefaultError, TLoginParams>({
-    mutationFn: async (data) => {
-      const response = await ServiceApi.AuthApi.login(data);
-      return response.data;
-    },
+  return useMutation({
+    mutationFn: ServiceApi.AuthApi.login,
   });
 };
