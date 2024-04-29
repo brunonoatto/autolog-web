7d5abfda3dc9a6edcaa8fc3b8036390e51f43de9
brunonoatto <brunonoatto@gmail.com>
2024-04-29 15:11:51 -0300

    fix: renomeando usuários de teste

--- src/e2e/shared/consts/auth.ts
+++ src/e2e/shared/consts/auth.ts
@@ -2,14 +2,14 @@ import { type TAccessTokenData } from '@core/api/auth/types';
 
 export const garageAccessTokenData: TAccessTokenData = {
   id: '54d77a26-d07a-4010-b0b3-9f5ad8139bbf',
-  name: 'primeira garagem',
+  name: 'Garagem do Zeca',
   type: 'garage',
 };
 export const garageAccessToken = `.${btoa(JSON.stringify(garageAccessTokenData))}.`;
 
 export const clientAccessTokenData: TAccessTokenData = {
   id: '5f182bd8-501c-4a84-803a-933cd253d2a8',
-  name: 'Joana Silva CNPJ',
+  name: 'Joao Client CNPJ',
   type: 'client',
 };
 
