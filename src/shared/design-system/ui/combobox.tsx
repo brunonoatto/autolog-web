b1abfb9972056070ff98fe882d608400ec87202a
brunonoatto <brunonoatto@gmail.com>
2024-05-02 19:59:36 -0300

    chore: remoção de comentário

--- src/shared/design-system/ui/combobox.tsx
+++ src/shared/design-system/ui/combobox.tsx
@@ -109,52 +109,10 @@ export default function Combobox<T extends FieldValues>({
               </Command>
             </PopoverContent>
           </Popover>
-          {/* <FormDescription>This is the item that will be used in the dashboard.</FormDescription> */}
+          {/* <FormDescription></FormDescription> */}
           <FormMessage />
         </FormItem>
       )}
     />
   );
-
-  // return (
-  //   <Popover open={open} onOpenChange={setOpen}>
-  //     <PopoverTrigger asChild>
-  //       <Button
-  //         variant="outline"
-  //         role="combobox"
-  //         aria-expanded={open}
-  //         className="w-[200px] justify-between"
-  //       >
-  //         {value ? items.find((item) => item.value === value)?.label : 'Selecione um item...'}
-  //         <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-  //       </Button>
-  //     </PopoverTrigger>
-  //     <PopoverContent className="w-[200px] p-0">
-  //       <Command>
-  //         <CommandInput {...otherProps} ref={ref} placeholder="Digite para procurar..." />
-  //         <CommandEmpty>No item found.</CommandEmpty>
-  //         <CommandGroup>
-  //           {items.map((item) => (
-  //             <CommandItem
-  //               key={item.value}
-  //               value={item.value}
-  //               onSelect={(currentValue) => {
-  //                 onChange?.(currentValue === value ? '' : currentValue);
-  //                 setOpen(false);
-  //               }}
-  //             >
-  //               <Check
-  //                 className={cn(
-  //                   'mr-2 h-4 w-4',
-  //                   value === item.value ? 'opacity-100' : 'opacity-0',
-  //                 )}
-  //               />
-  //               {item.label}
-  //             </CommandItem>
-  //           ))}
-  //         </CommandGroup>
-  //       </Command>
-  //     </PopoverContent>
-  //   </Popover>
-  // );
 }
