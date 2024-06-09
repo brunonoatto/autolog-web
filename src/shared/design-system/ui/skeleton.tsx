d59d55a1d805abab922603d3b368b77b68169534
brunonoatto <brunonoatto@gmail.com>
2024-06-09 18:58:06 -0300

    feat: add skeleton component

---
+++ src/shared/design-system/ui/skeleton.tsx
@@ -0,0 +1,7 @@
+import { cn } from '@shared/design-system/helpers/utils';
+
+function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={cn('animate-pulse rounded-md bg-muted', className)} {...props} />;
+}
+
+export { Skeleton };
