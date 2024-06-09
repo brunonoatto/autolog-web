cc47ec538e15222550f6c184b0dbf75d01998a0d
brunonoatto <brunonoatto@gmail.com>
2024-06-09 18:47:55 -0300

    feat: update auth api

--- src/core/api/auth/index.ts
+++ src/core/api/auth/index.ts
@@ -2,7 +2,9 @@ import httpClient from '@core/api/http-client';
 
 import type { TLoginParams, TLoginResponse } from './types';
 
+const BASE_URL = '/auth';
+
 export const login = async (data: TLoginParams): Promise<TLoginResponse> => {
-  const response = await httpClient.post<TLoginResponse>('/login', data);
+  const response = await httpClient.post<TLoginResponse>(`${BASE_URL}/login`, data);
   return response.data;
 };
