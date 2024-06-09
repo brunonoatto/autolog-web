0a69643ee7633d9d13d28123252c7a592c239257
brunonoatto <brunonoatto@gmail.com>
2024-06-09 20:28:55 -0300

    feat: remove observation in BudgetCard

--- src/shared/components/budget-card/index.tsx
+++ src/shared/components/budget-card/index.tsx
@@ -4,7 +4,6 @@ import type { TCar } from '@core/api/car/types';
 import useAuth from '@core/store/context/AuthContext/hook';
 import CarInfo from '@shared/components/car-info';
 import StatusBadge from '@shared/components/status-badge';
-import { CardTitle } from '@shared/design-system/ui/card';
 import { BudgetStatusEnum } from '@shared/types/budgetStatus';
 
 // TODO: melhorar a utilização desse componente pra não precisar ser todos campos opcionais
@@ -13,7 +12,6 @@ type TBudgetCardProps = {
   clientName?: string;
   garageName?: string;
   status?: BudgetStatusEnum;
-  observation?: string;
   createdDate?: string;
   hover?: boolean;
 };
@@ -23,7 +21,6 @@ export default function BudgetCard({
   clientName,
   garageName,
   status,
-  observation,
   createdDate,
   hover = false,
 }: TBudgetCardProps) {
@@ -37,22 +34,21 @@ export default function BudgetCard({
     <div className="space-y-4">
       <div
         data-hover={hover}
-        className="w-full rounded-lg ring-1 ring-primary data-[hover=true]:hover:ring-2 p-2 md:p-3 flex flex-col duration-300 data-[hover=true]:hover:scale-[1.01]"
+        className="w-full rounded-lg ring-1 ring-primary data-[hover=true]:hover:ring-2 p-2 md:p-3 flex flex-col duration-300 data-[hover=true]:hover:bg-muted"
       >
-        {tokenData?.type === 'garage' && clientName && (
+        {tokenData?.type === 'Garage' && clientName && (
           <div className="flex justify-between text-sm">
             <p className="whitespace-nowrap overflow-hidden text-ellipsis">Cliente: {clientName}</p>
             <p>{createdDate}</p>
           </div>
         )}
 
-        {tokenData?.type === 'client' && garageName && (
+        {tokenData?.type === 'Client' && garageName && (
           <div className="flex justify-between text-sm">
             <p className="whitespace-nowrap overflow-hidden text-ellipsis">Ofinina: {garageName}</p>
             <p>{createdDate}</p>
           </div>
         )}
-
         <div className="flex justify-between">
           <div>
             <CarInfo {...car} />
@@ -65,15 +61,6 @@ export default function BudgetCard({
           </div>
         </div>
       </div>
-
-      {observation && (
-        <>
-          <CardTitle icon="notebook-pen" size="lg">
-            Observação
-          </CardTitle>
-          {observation}
-        </>
-      )}
     </div>
   );
 }
