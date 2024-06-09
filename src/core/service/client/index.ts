2eba65d69e26e68cf8a0fc8f3cce58947ad8ee05
brunonoatto <brunonoatto@gmail.com>
2024-06-09 19:30:36 -0300

    feat: remove service Create Client

--- src/core/service/client/index.ts
+++ src/core/service/client/index.ts
@@ -1,3 +1,2 @@
 export * from './types';
 export * from './useClientCars';
-export * from './useCreateClient';
--- src/core/service/client/useCreateClient.ts
+++
@@ -1,9 +0,0 @@
-import { useMutation } from '@tanstack/react-query';
-
-import { ServiceApi } from '@core/api';
-
-export const useCreateClient = () => {
-  return useMutation({
-    mutationFn: ServiceApi.ClientApi.post,
-  });
-};
