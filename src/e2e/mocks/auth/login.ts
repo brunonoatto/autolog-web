d549e068f7af16b7092b514541e0bdabd428e542
brunonoatto <brunonoatto@gmail.com>
2024-06-09 19:35:56 -0300

    update userType in e2e

--- src/e2e/mocks/auth/login.ts
+++ src/e2e/mocks/auth/login.ts
@@ -3,14 +3,14 @@ import { TAccessTokenData } from '@core/api/auth/types';
 export const garageAccessTokenData: TAccessTokenData = {
   id: '54d77a26-d07a-4010-b0b3-9f5ad8139bbf',
   name: 'Garagem do Zeca',
-  type: 'garage',
+  type: 'Garage',
 };
 export const garageAccessToken = `.${btoa(JSON.stringify(garageAccessTokenData))}.`;
 
 export const clientAccessTokenData: TAccessTokenData = {
   id: '5f182bd8-501c-4a84-803a-933cd253d2a8',
   name: 'Joao Client CNPJ',
-  type: 'client',
+  type: 'Client',
 };
 
 export const clientAccessToken = `.${btoa(JSON.stringify(clientAccessTokenData))}.`;
