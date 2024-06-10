1e398c2cc34478026ab538423dfefd4374049f32
brunonoatto <brunonoatto@gmail.com>
2024-06-10 18:29:39 -0300

    feat: include loading e notFound components in GarageBudgetView

--- src/modules/garage/budget-view/index.tsx
+++ src/modules/garage/budget-view/index.tsx
@@ -1,22 +1,31 @@
-import { useGetBudget } from '@core/service/budget';
+import { useParams } from 'react-router-dom';
+
+import { useGetBudget, useObservationUpdate } from '@core/service/budget';
 import { BudgetViewProvider } from '@core/store/context/BudgetViewContext';
 import BudgetViewActions from '@modules/garage/budget-view/actions';
 import { GARAGE_BUDGET_VIEW_CARD_TEST_ID } from '@modules/garage/budget-view/consts';
 import BudgetViewForm from '@modules/garage/budget-view/form';
 import BudgetCard from '@shared/components/budget-card';
+import { BudgetObservation } from '@shared/components/budget-observation';
 import BudgetTable from '@shared/components/budget-table';
+import GoBackButton from '@shared/components/go-back-button';
+import LoadingCard from '@shared/components/loading-card';
+import { Alert, AlertDescription, AlertTitle } from '@shared/design-system/ui/alert';
 import { Card, CardContent, CardHeader, CardTitle } from '@shared/design-system/ui/card';
 import { BudgetStatusEnum } from '@shared/types/budgetStatus';
 
 function GarageBudgetViewContent() {
-  const { budget } = useGetBudget();
-  const { status, car, observation } = budget || {};
+  const { os: osParam } = useParams();
+  const { mutate: observationUpdateMutate, isPending } = useObservationUpdate(osParam);
+
+  const { budget, isLoading } = useGetBudget();
+  const { id, status, car, observation } = budget || {};
 
   const allowEditBudget = status === BudgetStatusEnum.MakingBudget;
 
-  if (!budget) {
-    return <>Loading...</>;
-  }
+  const handleObservationSave = (newObservation: string) => {
+    observationUpdateMutate({ budgetId: id!, observation: newObservation });
+  };
 
   return (
     <Card data-testid={GARAGE_BUDGET_VIEW_CARD_TEST_ID}>
@@ -24,13 +33,35 @@ function GarageBudgetViewContent() {
         <CardTitle>Orçamento</CardTitle>
       </CardHeader>
       <CardContent>
-        <BudgetCard status={status} car={car} observation={observation} />
+        <LoadingCard />
+        {isLoading && <LoadingCard />}
 
-        {allowEditBudget && <BudgetViewForm />}
+        {!isLoading && !budget && (
+          <Alert>
+            <AlertTitle>Orçamento {osParam} não encontrado.</AlertTitle>
+            <AlertDescription>
+              <GoBackButton />
+            </AlertDescription>
+          </Alert>
+        )}
 
-        <BudgetTable allowActions={allowEditBudget} />
+        {!isLoading && budget && (
+          <>
+            <BudgetCard status={status} car={car} />
 
-        <BudgetViewActions />
+            <BudgetObservation
+              observation={observation}
+              isLoading={isPending}
+              onEditedCallback={handleObservationSave}
+            />
+
+            {allowEditBudget && <BudgetViewForm />}
+
+            <BudgetTable allowActions={allowEditBudget} />
+
+            <BudgetViewActions />
+          </>
+        )}
       </CardContent>
     </Card>
   );
