867c642ee9164ec63cbff07f0dfba60fef4c62f4
brunonoatto <brunonoatto@gmail.com>
2024-06-11 20:06:49 -0300

    feat: use RenderLoadingData in BudgetsCar component of client CarView

--- src/modules/client/car-view/budgets-car/index.tsx
+++ src/modules/client/car-view/budgets-car/index.tsx
@@ -2,26 +2,19 @@ import { useParams } from 'react-router-dom';
 
 import { useListBudgetsCar } from '@core/service/budget';
 import { Item } from '@modules/client/car-view/budgets-car/item';
-import LoadingCard from '@shared/components/loading-card';
-import { Alert, AlertTitle } from '@shared/design-system/ui/alert';
+import { RenderLoadingData } from '@shared/components/render-loading-data';
 
 export default function BudgetsCar() {
   const { license: licenseParam } = useParams();
   const { budgets, isLoading } = useListBudgetsCar(licenseParam || '');
 
-  if (isLoading) {
-    return <LoadingCard />;
-  }
-
-  if (!budgets.length) {
-    return (
-      <Alert>
-        <AlertTitle>Este veículo ainda não realizou nenhum Orçamento.</AlertTitle>
-      </Alert>
-    );
-  }
-
   return (
-    <div className="space-y-2">{budgets?.map((item) => <Item key={item.os} item={item} />)}</div>
+    <RenderLoadingData
+      isLoading={isLoading}
+      hasData={!!budgets.length}
+      notFoundTitle="Este veículo ainda não realizou nenhum Orçamento."
+    >
+      <div className="space-y-2">{budgets?.map((item) => <Item key={item.os} item={item} />)}</div>
+    </RenderLoadingData>
   );
 }
