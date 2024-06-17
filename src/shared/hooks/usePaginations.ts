132a4e82c344ef09619c041b8be2e102199563ef
brunonoatto <brunonoatto@gmail.com>
2024-06-16 21:04:32 -0300

    feat: create goto next and previous page functions to usePagination

--- src/shared/hooks/usePaginations.ts
+++ src/shared/hooks/usePaginations.ts
@@ -1,14 +1,23 @@
 import { useMemo } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
+const DEFAULT_PAGE_NUMBER = 1;
+const DEFAULT_PAGE_SIZE = 10;
+
 export function usePagination() {
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const pageNumber = useMemo(() => Number(searchParams.get('page')) || 1, [searchParams]);
-  const pageSize = useMemo(() => Number(searchParams.get('pageSize')) || 10, [searchParams]);
+  const pageNumber = useMemo(
+    () => Number(searchParams.get('page') || 0) || DEFAULT_PAGE_NUMBER,
+    [searchParams],
+  );
+  const pageSize = useMemo(
+    () => Number(searchParams.get('pageSize') || 0) || DEFAULT_PAGE_SIZE,
+    [searchParams],
+  );
 
   const setPageNumber = (page: number) => {
-    if (page) {
+    if (page > 0) {
       setSearchParams({ page: page.toString() });
     } else {
       // TODO: criar um hook custom para ser mais fácil de deletar uma queryString
@@ -19,8 +28,20 @@ export function usePagination() {
     }
   };
 
+  const handlePreviousPage = () => {
+    if (pageNumber <= 1) return;
+
+    setPageNumber(pageNumber - 1);
+  };
+
+  const handleNextPage = () => {
+    // if (pageNumber >= pageSize) return;
+
+    setPageNumber(pageNumber + 1);
+  };
+
   const setPageSize = (size: number) => {
-    if (size) {
+    if (size > 0) {
       setSearchParams({ size: size.toString() });
     } else {
       setSearchParams((prev) => {
@@ -34,6 +55,8 @@ export function usePagination() {
     pageNumber,
     pageSize,
     setPageNumber,
+    onPreviousPage: handlePreviousPage,
+    onNextPage: handleNextPage,
     setPageSize,
   };
 }
