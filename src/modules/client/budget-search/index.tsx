6ba4c5da00f0bc98746857cded15874bf80966dd
brunonoatto <brunonoatto@gmail.com>
2024-06-17 19:28:59 -0300

    feat: use RenderLoadingData in BudgetSearch of client

--- src/modules/client/budget-search/index.tsx
+++ src/modules/client/budget-search/index.tsx
@@ -2,15 +2,15 @@ import { useSearchParams } from 'react-router-dom';
 
 import { useLicenseSearchBudgets } from '@core/service/budget';
 import BudgetCard from '@shared/components/budget-card';
+import { RenderLoadingData } from '@shared/components/render-loading-data';
 import ClientCarSelect from '@shared/components/selects/client-cars';
-import { Alert, AlertTitle } from '@shared/design-system/ui/alert';
 import { Card, CardContent, CardHeader, CardTitle } from '@shared/design-system/ui/card';
 import useNavigateCustom from '@shared/hooks/useNavigateCustom';
 
 export default function ClientBudgetSearch() {
   const navigate = useNavigateCustom();
   const [, setSearchParams] = useSearchParams();
-  const { budgets } = useLicenseSearchBudgets();
+  const { budgets, isLoading, totalPages, totalItems } = useLicenseSearchBudgets();
 
   const handleGoToBudget = (os: string) => {
     navigate(['/cliente/orcamento', os]);
@@ -36,25 +36,29 @@ export default function ClientBudgetSearch() {
       <CardContent className="space-y-4">
         <ClientCarSelect label="Veículo" onValueChange={handleLicenseFilterChange} />
 
-        {!budgets.length && (
-          <Alert>
-            <AlertTitle>Nenhum orçamento encontrado.</AlertTitle>
-          </Alert>
-        )}
-
-        {budgets?.map(({ os, garageName, createdDate, status, car }) => {
-          return (
-            <button key={os} className="w-full" onClick={() => handleGoToBudget(os)}>
-              <BudgetCard
-                hover
-                garageName={garageName}
-                car={car}
-                status={status}
-                createdDate={createdDate}
-              />
-            </button>
-          );
-        })}
+        <RenderLoadingData
+          isLoading={isLoading}
+          hasData={!!budgets}
+          notFoundTitle="Nenhum orçamento encontrado."
+          pagination={{
+            totalPages,
+            totalItems,
+          }}
+        >
+          {budgets?.map(({ os, garageName, createdDate, status, car }) => {
+            return (
+              <button key={os} className="w-full" onClick={() => handleGoToBudget(os)}>
+                <BudgetCard
+                  hover
+                  garageName={garageName}
+                  car={car}
+                  status={status}
+                  createdDate={createdDate}
+                />
+              </button>
+            );
+          })}
+        </RenderLoadingData>
       </CardContent>
     </Card>
   );
