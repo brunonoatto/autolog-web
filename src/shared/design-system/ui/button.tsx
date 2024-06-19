0aacf38fc895fa4b2022ba0696b19c04eb29efa1
brunonoatto <brunonoatto@gmail.com>
2024-06-19 19:48:32 -0300

    feat: add seccondIcon in Button to display icon to the right on the text Button

--- src/shared/design-system/ui/button.tsx
+++ src/shared/design-system/ui/button.tsx
@@ -10,6 +10,12 @@ export const buttonVariants = cva(
   'inline-flex gap-2 items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-70',
   {
     variants: {
+      size: {
+        default: 'h-10 px-4 py-2',
+        sm: 'h-8 rounded-md px-2',
+        lg: 'h-11 rounded-md px-8',
+        icon: 'h-10 w-10',
+      },
       variant: {
         default: 'bg-border text-primary-foreground hover:bg-border/90',
         destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
@@ -18,13 +24,7 @@ export const buttonVariants = cva(
           'border-2 border-input bg-background text-primary hover:bg-accent hover:text-accent-foreground',
         secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline',
-      },
-      size: {
-        default: 'h-10 px-4 py-2',
-        sm: 'h-8 rounded-md px-2',
-        lg: 'h-11 rounded-md px-8',
-        icon: 'h-10 w-10',
+        link: 'text-primary underline-offset-4 hover:underline p-0',
       },
     },
     defaultVariants: {
@@ -42,6 +42,8 @@ export interface TButtonProps
   size?: 'default' | 'sm' | 'lg' | 'icon';
   icon?: TIcons;
   iconProps?: TIconProps;
+  secondIcon?: TIcons;
+  secondIconProps?: TIconProps;
 }
 
 export const Button = React.forwardRef<HTMLButtonElement, TButtonProps>(
@@ -56,6 +58,8 @@ export const Button = React.forwardRef<HTMLButtonElement, TButtonProps>(
       asChild = false,
       icon,
       iconProps,
+      secondIcon,
+      secondIconProps,
       ...props
     },
     ref,
@@ -75,6 +79,8 @@ export const Button = React.forwardRef<HTMLButtonElement, TButtonProps>(
         {icon && !isLoading && <Icon name={icon} size={iconSize} {...iconProps} />}
 
         {children}
+
+        {secondIcon && <Icon name={secondIcon} size={iconSize} {...secondIconProps} />}
       </Comp>
     );
   },
