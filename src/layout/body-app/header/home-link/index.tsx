63b4bba4517daf280283bddf5f1b10fda86120a7
brunonoatto <brunonoatto@gmail.com>
2024-06-23 19:36:23 -0300

    style: allow send center prop to align component

--- src/layout/body-app/header/home-link/index.tsx
+++ src/layout/body-app/header/home-link/index.tsx
@@ -1,18 +1,33 @@
+import { cva, type VariantProps } from 'class-variance-authority';
 import { Link } from 'react-router-dom';
-import { twMerge } from 'tailwind-merge';
 
 import useAuth from '@core/store/context/AuthContext/hook';
+import { cn } from '@shared/design-system/helpers/utils';
 
-type THomeLink = { className?: string };
-const HomeLink = ({ className }: THomeLink) => {
+const homeLinkVariants = cva(
+  'block max-w-min text-2xl font-bold text-primary rounded-lg border-2 px-4 py-2 shadow-sm shadow-teal-100 hover:shadow-teal-500',
+  {
+    variants: {
+      center: {
+        true: 'm-auto',
+      },
+    },
+    defaultVariants: {
+      center: false,
+    },
+  },
+);
+
+type THomeLink = VariantProps<typeof homeLinkVariants> & {
+  className?: string;
+};
+
+const HomeLink = ({ className, center }: THomeLink) => {
   const { isAuthenticated } = useAuth();
 
   return (
     <Link
-      className={twMerge(
-        'block max-w-min text-2xl font-bold text-primary rounded-lg border-2 px-4 py-2 shadow-sm shadow-teal-100 hover:shadow-teal-500',
-        className,
-      )}
+      className={cn(homeLinkVariants({ center }), className)}
       to={isAuthenticated ? '/garage' : '/'}
     >
       AutoLog
