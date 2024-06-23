1748ba416c62d59291afe9257e87603ed421bf90
brunonoatto <brunonoatto@gmail.com>
2024-06-23 19:17:36 -0300

    feat: create component to reject budget to user without login

--- src/modules/without-login/budget-rejected/index.tsx
+++ src/modules/without-login/budget-rejected/index.tsx
@@ -1,3 +1,31 @@
+import HomeLink from '@layout/body-app/header/home-link';
+import { ClientRegisterCard } from '@shared/components/client-register-card';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@shared/design-system/ui/card';
+
 export default function BudgetRejected() {
-  return <div>Orçamento Rejeitado!</div>;
+  return (
+    <div className="w-full max-w-[650px] pt-4 px-4 md:px-0 mx-auto space-y-4 ">
+      <HomeLink />
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Orçamento Rejeitado!</CardTitle>
+          <CardDescription>
+            Você rejeitou o orçamento enviado, já enviamos para a mecânica realizar as alterações
+            solicitadas.
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <ClientRegisterCard />
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
