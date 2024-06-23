cfaa0ec256cd6b896ce8c07c4ddd1c00629d4dc9
brunonoatto <brunonoatto@gmail.com>
2024-06-23 19:01:53 -0300

    style: add primary variant in Alert

--- src/shared/design-system/ui/alert.tsx
+++ src/shared/design-system/ui/alert.tsx
@@ -11,6 +11,7 @@ const alertVariants = cva(
         default: 'bg-background text-foreground',
         destructive:
           'border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive',
+        primary: 'bg-primary text-muted',
       },
     },
     defaultVariants: {
