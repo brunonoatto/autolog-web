5c539801a049a01d127c18b310ab3ba1189e4878
brunonoatto <brunonoatto@gmail.com>
2024-06-23 19:16:58 -0300

    feat: create component to success approve budget to user without login

--- src/modules/without-login/budget-approved/index.tsx
+++ src/modules/without-login/budget-approved/index.tsx
@@ -1,3 +1,27 @@
+import HomeLink from '@layout/body-app/header/home-link';
+import { ClientRegisterCard } from '@shared/components/client-register-card';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@shared/design-system/ui/card';
+
 export default function BudgetApproved() {
-  return <div>Orçamento Aprovado!</div>;
+  return (
+    <div className="w-full max-w-[650px] pt-4 px-4 md:px-0 mx-auto space-y-4 ">
+      <HomeLink />
+      <Card>
+        <CardHeader>
+          <CardTitle>Orçamento Aprovado!</CardTitle>
+          <CardDescription>Oba! Você aprovou o orçamento do seu veículo.</CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <ClientRegisterCard />
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
