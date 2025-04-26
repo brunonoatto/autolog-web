ec29cc34efb35ce3290574bba4aa8da733ded3cf
brunonoatto <brunonoatto@gmail.com>
2025-04-26 20:30:31 -0300

    test: adjust teste to pass

--- src/e2e/modules/auth/login/loginWithClient.spec.e2e.ts
+++ src/e2e/modules/auth/login/loginWithClient.spec.e2e.ts
@@ -1,4 +1,5 @@
 import { test } from '@e2e/core/fixtures';
+import { clientAccessTokenData } from '@e2e/mocks/auth/login';
 import { loginClientRouteData } from '@e2e/modules/auth/login/mocks';
 
 test('Deve realizar o login com usuário do tipo Client', async ({
@@ -14,6 +15,6 @@ test('Deve realizar o login com usuário do tipo Client', async ({
 
   await loginPage.login('client@client.com.br', 'senha123');
 
-  await headerPage.expectNameToUser('clientAccessTokenData.name');
+  await headerPage.expectNameToUser(clientAccessTokenData.name);
   await clientMyCarsPage.expectCardTitle('Meus Veículos');
 });
