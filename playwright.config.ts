2a1a8761bdece94f5d2a19ddadeaf97fa215eac8
brunonoatto <brunonoatto@gmail.com>
2025-04-26 20:11:36 -0300

    e2e: set url to localhost

--- playwright.config.ts
+++ playwright.config.ts
@@ -61,7 +61,7 @@ export default defineConfig({
     {
       // Build
       command: 'pnpm preview',
-      url: 'http://127.0.0.1:4173',
+      url: 'http://localhost:4173',
       // Local
       // command: 'pnpm start',
       // url: 'http://127.0.0.1:5173',
